3916342a7f4b38f0d799e918d34c434b8b7182fa
abdullahhosenakash <abdullahhosenakash@gmail.com>
2022-08-12 07:27:43 +0600

    code added

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ndhaz4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const wordCollection = client.db("lugatulQuran").collection("arabicDictionary");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', async (req, res) => {
+    res.send('Lugatul Quran Running');
+});
+
+app.listen(port, () => {
+    console.log('Lugatul Quran Running on port', 5000);
+});
